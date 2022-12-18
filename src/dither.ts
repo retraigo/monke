f8dfab2524d882a3dfa25d5fb4f80fe9cecb51e6
Neko Of The Abyss <psi@innocent.com>
2022-12-18 17:41:55 +0530

    try reverse order

--- src/dither.ts
+++ src/dither.ts
@@ -1,7 +1,7 @@
 import { Color, meanDistance } from "../deps/color.ts";
 
-/** 
- * Dither the image into monochrome 
+/**
+ * Dither the image into monochrome
  * Uses Floyd-Steinberg matrix
  */
 export function monochromeDither(pixels: Color[], width: number): Color[] {
@@ -41,7 +41,7 @@ function findClosestColor(color: Color, palette: Color[]): Color {
   };
   let i = 0;
   while (i < palette.length) {
-    const m = meanDistance(palette[i], color);
+    const m = meanDistance(color, palette[i]);
     if (m < closest.dist) {
       closest.dist = m;
       closest.i = i;
@@ -51,8 +51,8 @@ function findClosestColor(color: Color, palette: Color[]): Color {
   return palette[closest.i];
 }
 
-/** 
- * Dither the image into a smaller palette 
+/**
+ * Dither the image into a smaller palette
  * Very fast dithering
  * Use twoRowSierra for more accuracy
  * Use monochromeDither for monochrome dithering
@@ -68,9 +68,9 @@ export function floydSteinbergDither(
   while (i < (pixels.length)) {
     const newC = findClosestColor(pixels[i], palette);
     const err = {
-      r: Math.floor((pixels[i].r - newC.r) >> 4),
-      g: Math.floor((pixels[i].r - newC.g) >> 4),
-      b: Math.floor((pixels[i].r - newC.b) >> 4),
+      r: Math.floor((pixels[i].r - newC.r) / 16),
+      g: Math.floor((pixels[i].r - newC.g) / 16),
+      b: Math.floor((pixels[i].r - newC.b) / 16),
     };
 
     // Match our quantized palette
@@ -100,7 +100,7 @@ export function floydSteinbergDither(
   return pixels;
 }
 
-/** 
+/**
  * Dither the image into a smaller palette
  * Uses two-row Sierra matrix
  */
@@ -115,9 +115,9 @@ export function twoRowSierraDither(
   while (i < (pixels.length)) {
     const newC = findClosestColor(pixels[i], palette);
     const err = {
-      r: Math.floor((pixels[i].r - newC.r) >> 4),
-      g: Math.floor((pixels[i].r - newC.g) >> 4),
-      b: Math.floor((pixels[i].r - newC.b) >> 4),
+      r: Math.floor((pixels[i].r - newC.r) / 16),
+      g: Math.floor((pixels[i].r - newC.g) / 16),
+      b: Math.floor((pixels[i].r - newC.b) / 16),
     };
 
     // Match our quantized palette
@@ -126,9 +126,9 @@ export function twoRowSierraDither(
     // Spread error to neighbouring pixels
     if (i < pixels.length - 1) {
       // Spread error to next pixel
-      pixels[i + 1].r += err.r << 2;
-      pixels[i + 1].g += err.g << 2;
-      pixels[i + 1].b += err.b << 2;
+      pixels[i + 1].r += err.r * 4;
+      pixels[i + 1].g += err.g * 4;
+      pixels[i + 1].b += err.b * 4;
       if (i < pixels.length - 2) {
         // Spread error to next pixel
         pixels[i + 2].r += err.r * 3;
@@ -137,21 +137,21 @@ export function twoRowSierraDither(
         // Spread error to lower pixels
         if (i < pixels.length - width - 2) {
           pixels[i + width - 2].r += err.r;
-          pixels[i + width - 1].r += err.r << 1;
+          pixels[i + width - 1].r += err.r * 2;
           pixels[i + width].r += err.r * 3;
-          pixels[i + width + 1].r += err.r << 1;
+          pixels[i + width + 1].r += err.r * 2;
           pixels[i + width + 2].r += err.r;
 
           pixels[i + width - 2].g += err.g;
-          pixels[i + width - 1].g += err.g << 1;
+          pixels[i + width - 1].g += err.g * 2;
           pixels[i + width].g += err.g * 3;
-          pixels[i + width + 1].g += err.g << 1;
+          pixels[i + width + 1].g += err.g * 2;
           pixels[i + width + 2].g += err.g;
 
           pixels[i + width - 2].b += err.b;
-          pixels[i + width - 1].b += err.b << 1;
+          pixels[i + width - 1].b += err.b * 2;
           pixels[i + width].b += err.b * 3;
-          pixels[i + width + 1].b += err.b << 1;
+          pixels[i + width + 1].b += err.b * 2;
           pixels[i + width + 2].b += err.b;
         }
       }
@@ -161,8 +161,7 @@ export function twoRowSierraDither(
   return pixels;
 }
 
-
-/** 
+/**
  * Dither the image into a smaller palette
  * Uses Sierra lite matrix
  * Use twoRowSierraDither for more accuracy
@@ -172,10 +171,10 @@ export function quickDither(
   width: number,
   palette: Color[],
 ): Color[] {
-  let i = 0;
+  let i = pixels.length - 1;
   // pixels is an array of pixels with r, g, b values
   // width is the width of the image in pixels
-  while (i < (pixels.length)) {
+  while (i >= 0) {
     const newC = findClosestColor(pixels[i], palette);
     const err = {
       r: Math.floor((pixels[i].r - newC.r) >> 2),
@@ -187,29 +186,31 @@ export function quickDither(
     pixels[i] = newC;
 
     // Spread error to neighbouring pixels
-    if (i < pixels.length - 1) {
+    if (i > 0) {
       // Spread error to next pixel
-      pixels[i + 1].r += err.r << 1;
-      pixels[i + 1].g += err.g << 1;
-      pixels[i + 1].b += err.b << 1;
+      if (i % width !== 0) {
+        pixels[i - 1].r += err.r << 1;
+        pixels[i - 1].g += err.g << 1;
+        pixels[i - 1].b += err.b << 1;
+      }
       // Spread error to lower pixels
-      if (i < pixels.length - width - 1) {
-        pixels[i + width - 1].r += err.r;
-        pixels[i + width].r += err.r;
-        pixels[i + width - 1].g += err.g;
-        pixels[i + width].g += err.g;
-        pixels[i + width - 1].b += err.b;
-        pixels[i + width].b += err.b;
+      if (i > width + 1) {
+        pixels[i - width + 1].r += err.r;
+        pixels[i - width].r += err.r;
+        pixels[i - width + 1].g += err.g;
+        pixels[i - width].g += err.g;
+        pixels[i - width + 1].b += err.b;
+        pixels[i - width].b += err.b;
       }
     }
-    i += 1;
+    i -= 1;
   }
   return pixels;
 }
 
-/** 
+/**
  * Dither the image into a smaller palette
- * Uses a quick, two-row dither 
+ * Uses a quick, two-row dither
  * Use twoRowSierraDither for more accuracy
  */
 export function quickTwoRowDither(
@@ -218,16 +219,15 @@ export function quickTwoRowDither(
   palette: Color[],
 ): Color[] {
   let i = pixels.length - 1;
-  const twoW = width << 2
+  const twoW = width * 4;
   // pixels is an array of pixels with r, g, b values
   // width is the width of the image in pixels
   while (i >= 0) {
-
     const newC = findClosestColor(pixels[i], palette);
     const err = {
-      r: Math.floor((pixels[i].r - newC.r) >> 4),
-      g: Math.floor((pixels[i].r - newC.g) >> 4),
-      b: Math.floor((pixels[i].r - newC.b) >> 4),
+      r: Math.floor((pixels[i].r - newC.r) / 16),
+      g: Math.floor((pixels[i].r - newC.g) / 16),
+      b: Math.floor((pixels[i].r - newC.b) / 16),
     };
 
     // Match our quantized palette
@@ -236,40 +236,40 @@ export function quickTwoRowDither(
     // Spread error to neighbouring pixels
     if (i >= 1) {
       // Spread error to next pixel
-      pixels[i - 1].r += err.r << 2;
-      pixels[i - 1].g += err.g << 2;
-      pixels[i - 1].b += err.b << 2;
+      pixels[i - 1].r += err.r * 4;
+      pixels[i - 1].g += err.g * 4;
+      pixels[i - 1].b += err.b * 4;
       if (i >= 2) {
         // Spread error to next pixel
-        pixels[i - 2].r += err.r << 1;
-        pixels[i - 2].g += err.g << 1;
-        pixels[i - 2].b += err.b << 1;
+        pixels[i - 2].r += err.r * 2;
+        pixels[i - 2].g += err.g * 2;
+        pixels[i - 2].b += err.b * 2;
         // Spread error to lower pixels
         const k = i - width;
         if (k >= 2) {
           const k = i - width;
           pixels[k - 2].r += err.r;
-          pixels[k - 1].r += err.r << 1;
-          pixels[k].r += err.r << 1;
-          pixels[k + 1].r += err.r << 1;
+          pixels[k - 1].r += err.r * 2;
+          pixels[k].r += err.r * 2;
+          pixels[k + 1].r += err.r * 2;
           pixels[k + 2].r += err.r;
 
           pixels[k - 2].g += err.g;
-          pixels[k - 1].g += err.g << 1;
-          pixels[k].g += err.g << 1;
-          pixels[k + 1].g += err.g << 1;
+          pixels[k - 1].g += err.g * 2;
+          pixels[k].g += err.g * 2;
+          pixels[k + 1].g += err.g * 2;
           pixels[k + 2].g += err.g;
 
           pixels[k - 2].b += err.b;
-          pixels[k - 1].b += err.b << 1;
-          pixels[k].b += err.b << 1;
-          pixels[k + 1].b += err.b << 1;
+          pixels[k - 1].b += err.b * 2;
+          pixels[k].b += err.b * 2;
+          pixels[k + 1].b += err.b * 2;
           pixels[k + 2].b += err.b;
 
           if (i >= twoW + 1) {
-            pixels[i - twoW + 1].r += err.r << 1;
-            pixels[i - twoW + 1].g += err.g << 1;
-            pixels[i - twoW + 1].b += err.b << 1;
+            pixels[i - twoW + 1].r += err.r * 2;
+            pixels[i - twoW + 1].g += err.g * 2;
+            pixels[i - twoW + 1].b += err.b * 2;
           }
         }
       }
@@ -279,7 +279,7 @@ export function quickTwoRowDither(
   return pixels;
 }
 
-/** 
+/**
  * Dither the image into a smaller palette
  * Starts from the mid point of the image
  * I have no idea why I did this
@@ -295,9 +295,9 @@ export function midPointDither(
   while (j < (pixels.length)) {
     const newC = findClosestColor(pixels[j], palette);
     const err = {
-      r: Math.floor((pixels[j].r - newC.r) >> 4),
-      g: Math.floor((pixels[j].r - newC.g) >> 4),
-      b: Math.floor((pixels[j].r - newC.b) >> 4),
+      r: Math.floor((pixels[j].r - newC.r) / 16),
+      g: Math.floor((pixels[j].r - newC.g) / 16),
+      b: Math.floor((pixels[j].r - newC.b) / 16),
     };
 
     // Match our quantized palette
@@ -306,31 +306,31 @@ export function midPointDither(
     // Spread error to neighbouring pixels
     if (j < pixels.length - 1) {
       // Spread error to next pixel
-      pixels[j + 1].r += err.r << 2;
-      pixels[j + 1].g += err.g << 2;
-      pixels[j + 1].b += err.b << 2;
+      pixels[j + 1].r += err.r * 4;
+      pixels[j + 1].g += err.g * 4;
+      pixels[j + 1].b += err.b * 4;
       if (j < pixels.length - 2) {
         // Spread error to next pixel
-        pixels[j + 2].r += err.r << 1;
-        pixels[j + 2].g += err.g << 1;
-        pixels[j + 2].b += err.b << 1;
+        pixels[j + 2].r += err.r * 2;
+        pixels[j + 2].g += err.g * 2;
+        pixels[j + 2].b += err.b * 2;
         // Spread error to lower pixels
         if (j < pixels.length - width - 2) {
           pixels[j + width - 2].r += err.r;
-          pixels[j + width - 1].r += err.r << 1;
-          pixels[j + width].r += err.r << 2;
-          pixels[j + width + 1].r += err.r << 1;
+          pixels[j + width - 1].r += err.r * 2;
+          pixels[j + width].r += err.r * 4;
+          pixels[j + width + 1].r += err.r * 2;
           pixels[j + width + 2].r += err.r;
 
           pixels[j + width - 2].g += err.g;
-          pixels[j + width - 1].g += err.g << 1;
-          pixels[j + width].g += err.g << 2;
-          pixels[j + width + 1].g += err.g << 1;
+          pixels[j + width - 1].g += err.g * 2;
+          pixels[j + width].g += err.g * 4;
+          pixels[j + width + 1].g += err.g * 2;
           pixels[j + width + 2].g += err.g;
           pixels[j + width - 2].b += err.b;
-          pixels[j + width - 1].b += err.b << 1;
-          pixels[j + width].b += err.b << 2;
-          pixels[j + width + 1].b += err.b << 1;
+          pixels[j + width - 1].b += err.b * 2;
+          pixels[j + width].b += err.b * 4;
+          pixels[j + width + 1].b += err.b * 2;
           pixels[j + width + 2].b += err.b;
         }
       }
@@ -342,9 +342,9 @@ export function midPointDither(
   while (i > 0) {
     const newC = findClosestColor(pixels[i], palette);
     const err = {
-      r: Math.floor((pixels[i].r - newC.r) >> 4),
-      g: Math.floor((pixels[i].r - newC.g) >> 4),
-      b: Math.floor((pixels[i].r - newC.b) >> 4),
+      r: Math.floor((pixels[i].r - newC.r) / 16),
+      g: Math.floor((pixels[i].r - newC.g) / 16),
+      b: Math.floor((pixels[i].r - newC.b) / 16),
     };
 
     // Match our quantized palette
@@ -353,9 +353,9 @@ export function midPointDither(
     // Spread error to neighbouring pixels
     if (i >= 1) {
       // Spread error to next pixel
-      pixels[i - 1].r += err.r << 2;
-      pixels[i - 1].g += err.g << 2;
-      pixels[i - 1].b += err.b << 2;
+      pixels[i - 1].r += err.r * 4;
+      pixels[i - 1].g += err.g * 4;
+      pixels[i - 1].b += err.b * 4;
       if (i >= 2) {
         // Spread error to next pixel
         pixels[i - 2].r += err.r * 3;
@@ -364,21 +364,21 @@ export function midPointDither(
         // Spread error to lower pixels
         if (i >= width + 2) {
           pixels[i - width - 2].r += err.r;
-          pixels[i - width - 1].r += err.r << 1;
+          pixels[i - width - 1].r += err.r * 2;
           pixels[i - width].r += err.r * 3;
-          pixels[i - width + 1].r += err.r << 1;
+          pixels[i - width + 1].r += err.r * 2;
           pixels[i - width + 2].r += err.r;
 
           pixels[i - width - 2].g += err.g;
-          pixels[i - width - 1].g += err.g << 1;
+          pixels[i - width - 1].g += err.g * 2;
           pixels[i - width].g += err.g * 3;
-          pixels[i - width + 1].g += err.g << 1;
+          pixels[i - width + 1].g += err.g * 2;
           pixels[i - width + 2].g += err.g;
 
           pixels[i - width - 2].b += err.b;
-          pixels[i - width - 1].b += err.b << 1;
+          pixels[i - width - 1].b += err.b * 2;
           pixels[i - width].b += err.b * 3;
-          pixels[i - width + 1].b += err.b << 1;
+          pixels[i - width + 1].b += err.b * 2;
           pixels[i - width + 2].b += err.b;
         }
       }
