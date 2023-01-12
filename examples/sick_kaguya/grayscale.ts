f1bc57438ffec2211746cf68193fdbecefe8aba4
Neko Of The Abyss <psi@innocent.com>
2023-01-12 13:49:14 +0530

    use Image over manipulating individual pixels

--- examples/sick_kaguya/grayscale.ts
+++ examples/sick_kaguya/grayscale.ts
@@ -3,17 +3,14 @@ import { getPixels } from "../../mod.ts";
 
 const __dirname = new URL(".", import.meta.url).pathname;
 
-const pixels = await getPixels(`${__dirname}/a.png`);
+const image = await getPixels(`${__dirname}/a.png`);
 
-const newImg = pixels.pixels.map(x => x.grayscale)
+image.grayscale()
 
-const i = createCanvas(pixels.width, pixels.pixels.length / pixels.width);
+const i = createCanvas(image.width, image.height);
 
 const ctx = i.getContext("2d");
 
-const res = Uint8ClampedArray.from(
-  newImg.map(x => [x.r, x.g, x.b, x.a]).flat(),
-);
-ctx.putImageData({ data: res, width: i.width, height: i.height }, 0, 0);
+ctx.putImageData(image.toImageData(), 0, 0);
 
 Deno.writeFileSync(`${__dirname}/gray_kaguya.png`, i.toBuffer("image/png"));
