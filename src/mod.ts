fe86b2f557c26d51a1bca652943c796d91add763
retraigo <nett@nett.moe>
2023-03-27 22:42:40 +0530

    update exports

--- src/mod.ts
+++ src/mod.ts
@@ -1,12 +1,12 @@
 export * from "./quantize/mod.ts";
 
-export { ColorHistogram, getHistogram } from "./histogram.ts";
+export * from "./histogram.ts";
 
-export { getPixels } from "./pixels.ts";
+export * from "./pixels.ts";
 
 export * from "./dither/mod.ts";
 
-export { Image } from "./structures/image.ts";
+export * from "./structures/image.ts";
 
 // For backwards compat. TODO: Remove soon
 export { Color } from "../deps/color.ts";
